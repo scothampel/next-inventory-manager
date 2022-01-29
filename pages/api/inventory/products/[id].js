9707cc239f3f04a14120948d4779a7855ef50d1c
Scot Hampel <scot@scothampel.com>
2022-01-29 16:30:49 -0600

    find item[s] in inventory

---
+++ pages/api/inventory/products/[id].js
@@ -0,0 +1,9 @@
+export default async function handler(req, res) {
+  const { id } = req.query
+  const oId = new require("mongodb").ObjectId(id)
+
+  const dbClient = await require("../../../../lib/Db").connect()
+  const product = await dbClient.collection(process.env.PRODUCTS_TABLE).findOne({_id: oId})
+  
+  res.status(200).json(product)
+}
---
+++ pages/api/inventory/products/index.js
@@ -0,0 +1,6 @@
+export default async function handler(req, res) {
+  const dbClient = await require("../../../../lib/Db").connect()
+  const products = await dbClient.collection(process.env.PRODUCTS_TABLE).find().toArray()
+  
+  res.status(200).json(products)
+}
