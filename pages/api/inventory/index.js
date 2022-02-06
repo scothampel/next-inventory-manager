e39a39cdfd2aecbe89c572413a7ff42586cd38fb
Scot Hampel <scot@scothampel.com>
2022-02-06 11:08:07 -0600

    Remove products folder

--- pages/api/inventory/index.js
+++ pages/api/inventory/index.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const dbClient = await require("../../../../lib/Db").connect()
+  const dbClient = await require("../../../lib/Db").connect()
   const products = await dbClient.collection(process.env.PRODUCTS_TABLE).find().toArray()
   
   res.status(200).json(products)
