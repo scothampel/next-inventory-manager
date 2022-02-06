c29596903a7a40e3d7799824bd4c7be764f8cfb8
Scot Hampel <scot@scothampel.com>
2022-02-06 11:15:45 -0600

    Update ENV val

--- pages/api/inventory/[id].js
+++ pages/api/inventory/[id].js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   const oId = new require("mongodb").ObjectId(id)
 
   const dbClient = await require("../../../lib/Db").connect()
-  const product = await dbClient.collection(process.env.PRODUCTS_TABLE).findOne({_id: oId})
+  const product = await dbClient.collection(process.env.INVENTORY_TABLE).findOne({_id: oId})
   
   res.status(200).json(product)
 }
