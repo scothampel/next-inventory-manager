182b7e58f54bebba925108564797d4043b9ced73
Scot Hampel <scot@scothampel.com>
2022-02-07 16:04:12 -0600

    add api checks if name already exists error codes

--- pages/api/inventory/add.js
+++ pages/api/inventory/add.js
@@ -6,12 +6,16 @@ export default async function handler(req, res) {
 
     if (count) {
       if (name) {
-        const insert = await dbClient.collection(process.env.INVENTORY_TABLE).insertOne({ name, count: parseInt(count) })
-        res.status(200).json(insert)
+        const exists = await dbClient.collection(process.env.INVENTORY_TABLE).findOne({name})
+        if (exists) res.status(400).json({ message: "item with that name already exists", code: 0 })
+        else {
+          const insert = await dbClient.collection(process.env.INVENTORY_TABLE).insertOne({ name, count: parseInt(count) })
+          res.status(200).json(insert)
+        }
       }
-      else res.status(400).json({ message: "please provide a name for the item" })
+      else res.status(400).json({ message: "please provide a name for the item", code: 1 })
     }
-    else res.status(400).json({ message: "please provide an item count" })
+    else res.status(400).json({ message: "please provide an item count", code: 2 })
   }
-  catch { res.status(500).json({ message: "db error" }) }
+  catch { res.status(500).json({ message: "db error", code: 3 }) }
 }
